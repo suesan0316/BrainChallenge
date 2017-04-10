148d37abbeafa4e36a7ce6c95e6afe77289efc8f
shunsuke <shunsuke@SHUNSUKE-PC>
2017-04-10 21:27:04 +0900

    ゲームモデルのToStringをオーバーライド

--- BrainChallenge.Common/Client/ClientModel/GameModel.cs
+++ BrainChallenge.Common/Client/ClientModel/GameModel.cs
@@ -1,10 +1,44 @@
-﻿namespace BrainChallenge.Common.Client.ClientModel
+﻿using System.Text;
+
+namespace BrainChallenge.Common.Client.ClientModel
 {
+    /// <summary>
+    /// メニュー一覧で使用するモデル
+    /// </summary>
     public class GameModel
     {
+        /// <summary>
+        /// ゲームID
+        /// </summary>
         public int GameId { get; set; }
+        /// <summary>
+        /// ゲーム名
+        /// </summary>
         public string GameName { get; set; }
+        /// <summary>
+        /// ゲームアイコン画像名
+        /// </summary>
         public string Icon { get; set; }
+        /// <summary>
+        /// コントローラクラス名
+        /// </summary>
         public string Class { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("[");
+            sb.Append("GameId=" + GameId);
+            sb.Append(",");
+            sb.Append("GameName=" + GameName);
+            sb.Append(",");
+            sb.Append("Icon=" + Icon);
+            sb.Append(",");
+            sb.Append("Class=" + Class);
+            sb.Append("]");
+
+            return sb.ToString();
+        }
     }
 }
