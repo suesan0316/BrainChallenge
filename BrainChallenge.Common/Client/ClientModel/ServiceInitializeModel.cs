d0cfd85609d7705035cbcef941c1a04d4c5675ae
shunsuke <shunsuke@SHUNSUKE-PC>
2017-04-12 08:49:28 +0900

    サービス初期化クラスを追加

---
+++ BrainChallenge.Common/Client/ClientModel/ServiceInitializeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainChallenge.Common.Client.ClientModel
+{
+    /// <summary>
+    /// クライアントサービスの初期化時に使用するモデルクラス
+    /// </summary>
+    public class ServiceInitializeModel
+    {
+        /// <summary>
+        /// ローカルDBのパス
+        /// </summary>
+        public string dbPath { get; set; }
+    }
+}
---
+++ BrainChallenge.Common/Client/ClientService/InterFace/ServiceIinitializer.cs
@@ -0,0 +1,13 @@
+﻿using BrainChallenge.Common.Client.ClientModel;
+using BrainChallenge.Common.Data.Connection;
+
+namespace BrainChallenge.Common.Client.ClientService.InterFace
+{
+    public class ServiceIinitializer
+    {
+        public static void initialize(ServiceInitializeModel init)
+        {
+            ConnectionProvider.dbPath = init.dbPath;
+        }
+    }
+}
