61c5dc35c8831ce14fb32cfae04fe624b67e6f94
shunsuke <shunsuke@SHUNSUKE-PC>
2017-04-16 00:14:32 +0900

    ゲームスタートサービスの必要クラスを追加

---
+++ BrainChallenge.Common/Client/ClientService/InterFace/IGameStartService.cs
@@ -0,0 +1,9 @@
+﻿using BrainChallenge.Common.Client.ClientModel;
+
+namespace BrainChallenge.Common.Client.ClientService.InterFace
+{
+    internal interface IGameStartService
+    {
+        GameDetailModel GetGameDetail(int gameId);
+    }
+}
