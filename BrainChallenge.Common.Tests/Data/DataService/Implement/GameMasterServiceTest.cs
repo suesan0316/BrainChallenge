c9a2ce2effde18e357ad577bd2c654e58621f962
shunsuke <shunsuke@SHUNSUKE-PC>
2017-04-22 16:43:59 +0900

    WBS : 21120,21130 新規登録 担当者 : 村野悟志

---
+++ BrainChallenge.Common.Tests/Data/DataService/Implement/GameMasterServiceTest.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using BrainChallenge.Common.Data.DataService.Implement;
+using BrainChallenge.Common.Data.Entity.General;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using BrainChallenge.Common.Data.Entity.Master;
+
+#if DEBUG
+[assembly: InternalsVisibleTo("BrainChallenge.Common")]
+#endif
+
+namespace BrainChallenge.Common.Tests.Data.DataService.Implement
+{
+    [TestFixture]
+    class GameMasterServiceTest
+    {
+
+        private GameMasterService _serv;
+
+        [SetUp]
+        public void Setup()
+        {
+            _serv = new GameMasterService();
+        }
+
+
+        [TearDown]
+        public void Tear()
+        {
+        }
+
+        [Test]
+        public void SelectAllTest1()
+        {
+            Assert.True(_serv.Select().Count == TestData.GameMasterTestData.Count);
+        }
+        [Test]
+        public void SelectAllTest2()
+        {
+            var result = _serv.Select();
+
+            var selectString = "";
+            var testDataString = "";
+
+            result.ForEach(data => selectString += data.ToString());
+            TestData.GameMasterTestData.ForEach(data => testDataString += data.ToString());
+
+            Assert.True(selectString.Equals(testDataString));
+        }
+
+        [Test]
+        public void SelectAndTest1()
+        {
+            var result = _serv.Select(TestData.GameMasterTestData[0]);
+
+            Assert.True(result[0].ToString().Equals(TestData.GameMasterTestData[0].ToString()));
+
+        }
+        [Test]
+        public void SelectAndTest2()
+        {
+            var result = _serv.Select(new GameMasterEntity { GameId = 2, GameName = "記憶力ゲーム3", GameTypeId = 0, ScoreType = 0, TitleImage = "memory3_game_title", IconImage = "memory3_game_icon", Class = "MemoryGame3Controller", GameTime = 3L });
+
+            Assert.True(result[0].ToString().Equals(TestData.GameMasterTestData[2].ToString()));
+        }
+    }
+}
---
+++ BrainChallenge.Common/Data/DataService/Implement/GameMasterService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrainChallenge.Common.Data.Connection;
+using BrainChallenge.Common.Data.Entity.Master;
+using BrainChallenge.Common.Data.Service.Interface;
+
+namespace BrainChallenge.Common.Data.DataService.Implement
+{
+    public class GameMasterService : IMasterDataService<GameMasterEntity>
+    {
+        public List<GameMasterEntity> Select()
+        {
+            using (var con = ConnectionProvider.GetConnection())
+            {
+                var result = from record in con.Table<GameMasterEntity>() select record;
+
+                return result.Count() != 0 ? result.ToList() : null;
+            }
+        }
+
+        public List<GameMasterEntity> Select(GameMasterEntity t)
+        {
+            using (var con = ConnectionProvider.GetConnection())
+            {
+                var result = from record in con.Table<GameMasterEntity>() select record;
+
+                if (t.GameId != -1)
+                    result = result.Where(data => data.GameId == t.GameId);
+                if (t.GameName != null)
+                    result = result.Where(data => data.GameName.Equals(t.GameName));
+                if (t.GameTypeId != -1)
+                    result = result.Where(data => data.GameTypeId == t.GameTypeId);
+                if (t.ScoreType != -1)
+                    result = result.Where(data => data.ScoreType == t.ScoreType);
+                if (t.TitleImage != null)
+                    result = result.Where(data => data.TitleImage.Equals(t.TitleImage));
+                if (t.IconImage != null)
+                    result = result.Where(data => data.IconImage.Equals(t.IconImage));
+                if (t.Class != null)
+                    result = result.Where(data => data.Class.Equals(t.Class));
+                if (t.GameTime != -1)
+                    result = result.Where(data => data.GameTime == t.GameTime);
+
+                return result.Count() != 0 ? result.ToList() : null;
+            }
+        }
+    }
+}
