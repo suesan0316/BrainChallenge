dfd853c116aa21d0de18c5b634eaa4b04b91da34
shunsuke <shunsuke@SHUNSUKE-PC>
2017-05-07 11:25:35 +0900

    commit

--- BrainChallenge.Droid/Controller/MenuController.cs
+++ BrainChallenge.Droid/Controller/MenuController.cs
@@ -1,29 +1,56 @@
+using System;
+using System.Xml;
 using Android.App;
 using Android.Content;
+using Android.Content.Res;
 using Android.OS;
+using Android.Support.V4.View;
+using Android.Util;
+using Android.Views;
 using Android.Widget;
 
 namespace BrainChallenge.Droid.Controller
 {
-	[Activity(MainLauncher = true)]
+    [Activity(MainLauncher = true)]
     public class MenuController : Activity
-	{
-	    private TableLayout _menuTable;
+    {
+        private TableLayout _menuTable;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
-			//SetTheme(AppConst.targetTheme);
+            SetTheme(AppConst.targetTheme);
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Menu);
 
             _menuTable = FindViewById<TableLayout>(Resource.Id.menuTable);
 
-			/*selectMemoryGameButton.Click += delegate
-			{
-				var next = new Intent(this, typeof(GameStartController));
-				next.PutExtra("MyData", "DetectiveGameController");
-				StartActivity(next);
-			};*/
+            var GameListTitleLinear = Util.GenerateView<LinearLayout>(this, "GameListTitleLinear");
+            var GameTypeListTitle = Util.GenerateView<TextView>(this, "GameTypeListTitle");
+            
+            GameTypeListTitle.Text = "‹L‰¯—Í";
+            GameListTitleLinear.AddView(GameTypeListTitle);
+            _menuTable.AddView(GameListTitleLinear);
+
+            var GameListScroll = Util.GenerateView<HorizontalScrollView>(this, "GameListScroll");
+
+            var GameListLinaer = Util.GenerateView<LinearLayout>(this, "GameListLinaer");
+            var GameLinaer = Util.GenerateView<LinearLayout>(this, "GameLinaer");
+
+            var GameButton = Util.GenerateView<ImageButton>(this, "GameButton");
+            GameButton.SetBackgroundResource(Resource.Drawable.detective_game_icon);
+
+            var GameTitle = Util.GenerateView<TextView>(this, "GameTitle");
+            GameTitle.Text = "–ÀŽq‚ð’T‚¹!!";
+
+            GameLinaer.AddView(GameButton);
+            GameLinaer.AddView(GameTitle);
+
+            GameListLinaer.AddView(GameLinaer);
+
+            GameListScroll.AddView(GameListLinaer);
+
+            _menuTable.AddView(GameListScroll);
+
         }
     }
 }
---
+++ BrainChallenge.Droid/Util.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using Android.Content;
+using Android.Views;
+using Android.Widget;
+
+namespace BrainChallenge.Droid
+{
+    internal class Util
+    {
+        public  static T GenerateView<T>(Context context, string styleName) where T : View
+        {
+            var styleId = (int)typeof(Resource.Style).GetField(styleName).GetValue(null);
+
+            var viewType = typeof(T);
+            var args = new object[] { context, null, styleId, styleId };
+            //var args = new object[] { new ContextThemeWrapper(context, styleId) };
+            var view = (T)Activator.CreateInstance(viewType, args);
+
+            var xml = XDocument.Load(context.Assets.Open("Styles.xml"));
+
+            var styles = xml.Descendants("style");
+
+            var result = from n in styles where n.Attribute("name").Value.Equals(styleName) select n;
+
+            var items = result.Descendants("item");
+
+            var result2 = from n in items select n;
+
+            var list = result2.ToList();
+
+            var layout_height = -1;
+            var layout_width = -1;
+            var layout_marginBottom = 0;
+            var layout_marginLeft = 0;
+            var layout_marginRight = 0;
+            var layout_marginTop = 0;
+            var paddingLeft = 0;
+            var paddingTop = 0;
+            var paddingBottom = 0;
+            var paddingRight = 0;
+
+            foreach (var val in list)
+            {
+                var name = val.Attribute("name").Value;
+
+                if (name.Equals("android:layout_height"))
+                {
+                    if (!int.TryParse(val.Value, out layout_height))
+                    {
+                        if (val.Value.Equals("wrap_content"))
+                        {
+                            layout_height = -2;
+                        }
+                        else
+                        {
+                            layout_height = -1;
+                        }
+                    }
+                }else if (name.Equals("android:layout_width"))
+                {
+                    if (!int.TryParse(val.Value, out layout_width))
+                    {
+                        if (val.Value.Equals("wrap_content"))
+                        {
+                            layout_width = -2;
+                        }
+                        else
+                        {
+                            layout_width = -1;
+                        }
+
+                      }
+                }else if (name.Equals("android:layout_marginBottom"))
+                {
+                    int.TryParse(val.Value, out layout_marginBottom);
+                }
+                else if (name.Equals("android:layout_marginLeft"))
+                {
+                    int.TryParse(val.Value, out layout_marginLeft);
+                }
+                else if (name.Equals("android:layout_marginRight"))
+                {
+                    int.TryParse(val.Value, out layout_marginRight);
+                }
+                else if (name.Equals("android:layout_marginTop"))
+                {
+                    int.TryParse(val.Value, out layout_marginTop);
+                }
+                else if (name.Equals("android:paddingLeft"))
+                {
+                    int.TryParse(val.Value, out paddingLeft);
+                }
+                else if (name.Equals("android:paddingTop"))
+                {
+                    int.TryParse(val.Value, out paddingTop);
+                }
+                else if (name.Equals("android:paddingBottom"))
+                {
+                    int.TryParse(val.Value, out paddingBottom);
+                }
+                else if (name.Equals("android:paddingRight"))
+                {
+                    int.TryParse(val.Value, out paddingRight);
+                }
+            }
+
+            var layoutParams = new LinearLayout.LayoutParams(layout_width, layout_height);
+            
+            view.SetPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+            layoutParams.SetMargins(layout_marginLeft, layout_marginTop, layout_marginRight, layout_marginBottom);
+            
+            view.LayoutParameters = layoutParams;
+            return view;
+        }
+    }
+}
